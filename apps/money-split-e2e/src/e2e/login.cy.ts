c1c11026e7f952347992de903f50be8c32a1ee70
anze.kolsek <anze.kolsek@gmail.com>
2024-04-02 17:58:11 +0200

    lint

--- apps/money-split-e2e/src/e2e/login.cy.ts
+++ apps/money-split-e2e/src/e2e/login.cy.ts
@@ -1,31 +1,31 @@
-import { ACTIONS } from "../support/actions";
-import { LOGIN_FORM } from "../support/app.po";
+import { ACTIONS } from '../support/actions';
+import { LOGIN_FORM } from '../support/app.po';
 
 describe('Login', () => {
-    beforeEach(() => {
-        ACTIONS.deleteOwner('testuser');
-        cy.visit('/login'); // replace with the actual path to your login page
-        ACTIONS.registerOwner('testuser', 'testpassword');
-    });
-  
-    it('should login with valid credentials', () => {
-      LOGIN_FORM.login('testuser', 'testpassword');
-      cy.url().should('include', '/realm');
-    });
-  
-    it('should not login with invalid password', () => {
-      LOGIN_FORM.login('testuser', 'invalidpassword');
-      cy
-      .get('[data-cy="notification"]')
-      .should('be.visible')
-      .and('contain', 'UNAUTHORIZED')  
-    });
-
-    it('should not login with invalid username', () => {
-      LOGIN_FORM.login('invaliduser', 'invalidpassword')  
-      cy
-      .get('[data-cy="notification"]')
-      .should('be.visible')
-      .and('contain', 'RESOURCE_NOT_FOUND')  
-    });
+  beforeEach(() => {
+    ACTIONS.deleteOwner('testuser');
+    cy.visit('/login'); // replace with the actual path to your login page
+    ACTIONS.registerOwner('testuser', 'testpassword');
   });
+
+  it('should login with valid credentials', () => {
+    LOGIN_FORM.login('testuser', 'testpassword');
+    cy.url().should('include', '/realm');
+  });
+
+  it('should not login with invalid password', () => {
+    LOGIN_FORM.login('testuser', 'invalidpassword');
+    cy
+      .get('[data-cy="notification"]')
+      .should('be.visible')
+      .and('contain', 'UNAUTHORIZED');
+  });
+
+  it('should not login with invalid username', () => {
+    LOGIN_FORM.login('invaliduser', 'invalidpassword');
+    cy
+      .get('[data-cy="notification"]')
+      .should('be.visible')
+      .and('contain', 'RESOURCE_NOT_FOUND');
+  });
+});
