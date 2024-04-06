97e1d1b3a4ab52bd2627e974258b270aa23f21c0
anze.kolsek <anze.kolsek@gmail.com>
2024-04-06 17:25:21 +0200

    lint

--- apps/data-provider/src/modules/avatar.ts
+++ apps/data-provider/src/modules/avatar.ts
@@ -1,34 +1,34 @@
-import { AvatarData } from "@angular-monorepo/entities";
-import { lastInsertId, query } from "../connection/connection"
-import { insertSql, selectOneWhereSql } from "../connection/helper"
-import { AvatarEntity, EntityPropertyType } from "../types"
+import { AvatarData } from '@angular-monorepo/entities';
+import { lastInsertId, query } from '../connection/connection';
+import { insertSql, selectOneWhereSql } from '../connection/helper';
+import { AvatarEntity, EntityPropertyType } from '../types';
 
 export const AVATAR_SERVICE = {
-    createAvatar: async (
-        color: string,
-        url: string,
-    ) => {
-        await query(insertSql(
-            'Avatar',
-            AvatarEntity,
-            { color, url }
-        ));
+  createAvatar: async (
+    color: string,
+    url: string,
+  ) => {
+    await query(insertSql(
+      'Avatar',
+      AvatarEntity,
+      { color, url },
+    ));
 
-        const id = await lastInsertId();
+    const id = await lastInsertId();
 
-        return await AVATAR_SERVICE.getById(id);
-    },
-    getById: async (
-        id: number,
-    ) => {
-        const avatar = await selectOneWhereSql<AvatarData>(
-            'Avatar',
-            'id',
-            EntityPropertyType.ID,
-            id,
-            AvatarEntity,
-        );
+    return await AVATAR_SERVICE.getById(id);
+  },
+  getById: async (
+    id: number,
+  ) => {
+    const avatar = await selectOneWhereSql<AvatarData>(
+      'Avatar',
+      'id',
+      EntityPropertyType.ID,
+      id,
+      AvatarEntity,
+    );
 
-        return avatar;
-    }
-}
+    return avatar;
+  },
+};
